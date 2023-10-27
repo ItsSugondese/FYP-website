77501a3064eb4ea4ff3b3ea42691e89ea8b2025a
Rohan <lazybot5528@gmail.com>
2023-10-27 11:56:17 +0545

    drawer and sidenav working perfectly

---
+++ src/app/features/staff/manage-foods/manage-foods.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-foods',
+  templateUrl: './manage-foods.component.html',
+  styleUrls: ['./manage-foods.component.scss']
+})
+export class ManageFoodsComponent implements OnInit {
+  ngOnInit(): void {
+  }
+
+}
